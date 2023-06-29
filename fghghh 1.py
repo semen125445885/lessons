c5f44bb96861056367e99b039dd488d42d3913e4
semen125445885 <138097591+semen125445885@users.noreply.github.com>
2023-06-29 13:38:52 +0700

    Update fghghh 1.py
    сделал с помощью срезов

--- fghghh 1.py
+++ fghghh 1.py
@@ -1,21 +1,4 @@
+def pal(string):
+    return string == string[::-1]
 
-# def strcounter(s):
-#     print(set(s))
-#     for sym in set(s):
-#         count = 0
-#         for sub_sym in s:
-#             if sym == sub_sym:
-#                 count += 1
-#         print(sym, "-", count)
-#
-# strcounter("ssa")
-
-def strcounter(s):
-    sym_counter = {}
-    for sym in s:
-        sym_counter[sym] = sym_counter.get(sym,0) + 1
-
-    for sym, count in sym_counter.items():
-        print(sym, count)
-
-strcounter("bjnsfbnmrgsbjnsghjb")
+print(pal('jnjsfnjklsdgbjfbhfklshfd'))
